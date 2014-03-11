1a46709923890f2b4f60edfd590eb969631186f8
Samuli Seppänen <samuli@openvpn.net>
2014-03-11 11:13:45 +0000

    Added mysql::user and mysql::grant defines
    These two new defines support creating and removing mysql/mariadb users and
    privileges. Fairly extensive testing has gone into these defines to make sure
    they work as expected and without errors. Parts of the code are (C) Mikko
    Vilpponen 2012 and released under the same BSD license as the rest of this
    module.
    
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

---
+++ manifests/grant.pp
@@ -0,0 +1,58 @@
+#
+# == Define: mysql::grant
+#
+# Manage MySQL user privileges.
+#
+# This define supports both adding and removing privileges. Interestingly 
+# MySQL/MariaDB does not seem to mind if the database being granted access to 
+# does not exist; the end result is simply that there are privileges pointing to 
+# non-existing databases. For discussion about root logins to MySQL have a look 
+# at the mysql::user define.
+#
+# == Parameters
+#
+# [*status*]
+#   Status of the grant. Valid values 'present' and 'absent'. Defaults to 
+#   'present'.
+# [*user*]
+#   The MySQL username to grant access for.
+# [*host*]
+#   The hostname or IP part of the user definition. Defaults to 'localhost'. For 
+#   discussion have a look at the mysql::user define.
+# [*database*]
+#   The database to grant privileges to.
+# [*privileges*]
+#   A comma-separated (string) list of privileges given to the user database. 
+#   For example 'STATUS', 'ALL' or 'SELECT,UPDATE'. Defaults to 'USAGE' (=no 
+#   privileges) for safety.
+#
+define mysql::grant
+(
+    $status = 'present',
+    $user,
+    $host = 'localhost',
+    $database,
+    $privileges = 'USAGE'
+)
+{
+    include mysql::params
+
+    $params = '--defaults-extra-file=/root/.my.cnf'
+
+    if $status == 'present' {
+        # See mysql::user for rationale on the backticks and backslashes.
+        exec { "mysql-grant-${privileges}-for-${user}-to-${database}":
+            command => "${::mysql::params::mysql_executable} ${params} -e \"GRANT ${privileges} ON \\`${database}\\`.* TO '${user}'@'${host}';\"",
+            onlyif  => "${::mysql::params::mysql_executable} ${params} -e \"SHOW GRANTS FOR '${user}'@'${host}';\"",
+        }
+    } elsif $status == 'absent' {
+        exec { "mysql-revoke-${privileges}-for-${user}-to-${database}":
+            command => "${::mysql::params::mysql_executable} ${params} -e \"REVOKE ${privileges} ON \\`${database}\\`.* FROM '${user}'@'${host}';\"",
+            onlyif  => "${::mysql::params::mysql_executable} ${params} -e \"SHOW GRANTS FOR '${user}'@'${host}';\"",
+        }
+    } else {
+        notify { "Value of the \$status parameter (\"${status}\") in a mysql::grant resource is invalid. Supported values are 'present' and 'absent'.":
+            loglevel => warning,
+        }
+    }    
+}
---
+++ manifests/user.pp
@@ -0,0 +1,60 @@
+#
+# == Define: mysql::user
+#
+# Manage a MySQL user.
+#
+# This define supports both adding and removing users, but depends on the system 
+# root user being able to login to MySQL without entering a password; in 
+# practice, this is achieved by having a properly configured /root/.my.cnf.
+#
+# == Parameters
+#
+# [*status*]
+#   Status of the user. Currently only supported value is 'present' (default). 
+#   Later on 'absent' can be added fairly easily.
+# [*user*]
+#   The MySQL username. If omitted, resource $title is used.
+# [*password*]:
+#   User's password
+# [*host*]
+#   The hostname or IP from which connections are allowed for this user. This is 
+#   required because in MySQL 'john'@'localhost' is not the same user as, say, 
+#   'john'@'server'. Defaults to 'localhost'.
+#
+define mysql::user
+(
+    $status = 'present',
+    $user = '',
+    $password,
+    $host = 'localhost'
+)
+{
+
+    include mysql::params
+
+    # Set the username
+    $user_value = $user ? {
+        '' => $title,
+        default => $user
+    }
+
+    $params = '--defaults-extra-file=/root/.my.cnf'
+
+    if $status == 'present' {
+        # Note: Backtick notation needs to be escaped to prevent bach 
+        # interpreting it as command subtitution We need to escape the backslash 
+        # also, that's why there are double backslashes.
+        exec { "mysql-create-user-${user_value}-at-${host}":
+            command => "${::mysql::params::mysql_executable} ${params} -e \"GRANT USAGE ON *.* TO '${user_value}'@'${host}' IDENTIFIED BY '${password}';\"",
+        }
+    } elsif $status == 'absent' {
+        exec { "mysql-drop-user-${user_value}-at-${host}":
+            command => "${::mysql::params::mysql_executable} ${params} -e \"DROP USER '${user_value}'@'${host}';\"",
+            onlyif  => "${::mysql::params::mysql_executable} ${params} -e \"SHOW GRANTS FOR '${user_value}'@'${host}';\"",
+        }
+    } else {
+        notify { "Value of the \$status parameter (\"${status}\") for a mysql::user resource is invalid. Supported values are 'present' and 'absent'.":
+            loglevel => warning,
+        }
+    }
+}
